8e23201e9b558deea6a685071129bd1e850a77d9
Marshall House <marshall@4zend.com>
2014-03-30 04:31:04 -0500

    Update Module.php

--- Module.php
+++ Module.php
@@ -4,15 +4,21 @@ namespace Propel;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\ModuleManager;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
+     public function init(ModuleManager $moduleManager) {
+          // setup the autoloading
+          require_once 'vendor/autoload.php';
+
+          // setup Propel
+          require_once 'generated-conf/config.php';
+     }
+
      public function getAutoloaderConfig()
      {
          return array(
-             'Zend\Loader\ClassMapAutoloader' => array(
-                 __DIR__ . '/autoload_classmap.php',
-             ),
              'Zend\Loader\StandardAutoloader' => array(
                  'namespaces' => array(
                      __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
