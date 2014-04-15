ab24e50d9307595270d6a2ae3b7b334a21f7a3f9
Marshall House <marshall@4zend.com>
2014-04-14 21:35:24 -0500

    Update Module.php

--- Module.php
+++ Module.php
@@ -14,7 +14,7 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
         require_once 'vendor/propel/propel1/runtime/lib/Propel.php';
 
         // Add the generated 'classes' directory to the include path
-        set_include_path("data/propel/proxy/build/classes" . PATH_SEPARATOR . get_include_path());
+        set_include_path("data/zpropel/proxy/build/classes" . PATH_SEPARATOR . get_include_path());
      }
      
      public function onBootstrap(MvcEvent $e) {
